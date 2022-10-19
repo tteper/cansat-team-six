f80f5441af9e6ac2f7f9576558ea06609fe240ff
Tom Teper <32948577+tteper@users.noreply.github.com>
2022-10-18 19:37:35 -0500

    Main 1.0

--- main.ino
+++ main.ino
@@ -7,25 +7,32 @@
  *  Copyright (c) Space Hardware Club, 2022
  */
 
+#define VERSION 1.0
+
 // sensor testing code
 #include <Wire.h>
 #include <SPI.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_BMP3XX.h"
 #include <Adafruit_GPS.h>
-#include <String>
+#include <string.h>
 #include <SoftwareSerial.h>
 #include <Servo.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 // configure constants
 #define SEALEVELPRESSURE_HPA 1013.25
 #define PMTK_SET_NMEA_UPDATE_5HZ  "$PMTK220,200*2C"
 #define PMTK_SET_NMEA_OUTPUT_RMCGGA "$PMTK314,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0*28"
 #define PMTK_Q_RELEASE "$PMTK605*31"
-#define REFRESH_RATE 200 // ms; 5 Hz
+
+// mission params
+#define REFRESH_RATE 1000 / 1 // ms; 5 Hz
+#define ALTITUDE_MAX 400 // altitude to trigger latch
 
 // DEBUG flag
-#define SERIAL_DEBUG false
+#define SERIAL_DEBUG true
 
 // global mission variables
 uint32_t TIMESTAMP = millis(); // time since POWER ON
@@ -43,24 +50,34 @@ const int LED = 3;              // LED Pico Pin -> 3
 Servo SERVO;                    // Servo Pico Pin -> 10
 
 // data struct for mission information
-struct {
+struct params {
   const int TEAM_ID = 1006;       // assigned team ID
   uint32_t MISSION_TIME;          // mission time in format hh:mm:ss.ss
   unsigned int PACKET_COUNT = 0;  // total count of transmitted packets
   unsigned int FLIGHT_STATE = 0;  // operating state of the software (0 <--> 4)
   char PL_STATE = 'N';            // payload state (N -> not released; R -> released)
   int ALTITUDE;                   // altitude in meters relative to ground level
+  float PRESSURE;                 // pressure in hPa
   float TEMP;                     // temperature in Celsius
   float VOLTAGE;                  
   double GPS_LAT;                 // latitude in decimal degrees
   double GPS_LON;                 // longitude in decimal degrees
-} MISSION_PARAMS;
+};
+
+// define MP structure
+params MISSION_PARAMS;
 
 void setup() {
-  
+
   // setup Serial connections
   Serial.begin(9600);   // USB: Computer Serial (UART)
-  Serial1.begin(9600);  // SD: Serial1
+  Serial1.begin(9600);  // XBee: Serial1 (UART0)
+  Serial2.begin(9600);  // SD: Serial2 (UART1)
+
+  // print version number
+  Serial.print("Version Number: ");
+  Serial.print(VERSION);
+  Serial.println();
   
   // GPS Configurations
   #ifdef SERIAL_DEBUG
@@ -92,35 +109,15 @@ void setup() {
   BMP.setOutputDataRate(BMP3_ODR_50_HZ);
   ALT_START = BMP.readAltitude(SEALEVELPRESSURE_HPA); // define altitude datum
 
-  // SD Configuration
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    while (1);
-  }
-
   // basic configuration variables
   String packetString = ""; // packet string
   START_TIME = TIMESTAMP;   // set the start time to current time for mission start
 
-  // SD: read three sensors and append to the string:
-  for (int analogPin = 0; analogPin < 3; analogPin++) {
-    int sensor = analogRead(analogPin);
-    packetString += String(sensor);
-    if (analogPin < 2) {
-      packetString += ",";
-    }
-  }
-
-  // Open file
-  File dataFile = SD.open("cansat-data.csv", FILE_WRITE);
+  // Create file header
   String header = "TEAM_ID, MISSION_TIME, PACKET_COUNT, FLIGHT_STATE, PL_STATE, ALTITUDE, TEMP, VOLTAGE, GPS_LAT, GPS_LONG";
 
-  // Write header
-  if (dataFile)
-    dataFile.println(header);
-  else 
-    return;
+  // Write header to SD
+  Serial2.println(header);
 
   // Servo configuration
   SERVO.attach(10); // set servo pin
@@ -130,67 +127,104 @@ void loop() {
   
   // set refresh rate (DEFAULT: 5Hz)
   delay(REFRESH_RATE);
-  
-  // configure file write as first action
-  if (!MISSION_START) {
-    
-  }
-  else {
 
-    // update mission_params
-    MISSION_PARAMS.MISSION_TIME = TIMESTAMP - START_TIME;
-    MISSION_PARAMS.PACKET_COUNT++;
-    MISSION_PARAMS.ALTITUDE = BMP.readAltitude(SEALEVELPRESSURE_HPA) - ALT_START;
-    MISSION_PARAMS.TEMP = BMP.readTemperature();
-    // MISSION_PARAMS.VOLTAGE = something
-    MISSION_PARAMS.GPS_LAT = GPS.latitude;
-    MISSION_PARAMS.GPS_LON = GPS.longitude;
-    packetString = csvParams(MISSION_PARAMS);
+  // create write string and mission params struct
+  String packetString;
 
-    // main execution code
-    switch (MISSION_PARAMS.FLIGHT_STATE) {
-      case 0: // IDLE
-        break;
-      case 1: // ASCENT
-        dataFile.println(packetString);
-        Serial1.println(packetString);
-        
-        // FS1 -> FS2
-        if (MISSION_PARAMS.ALTITUDE > 400) {
-          MISSION_PARAMS.FLIGHT_STATE = 2; // drop stage
-          // activate servo
-        }
-        break;
-      case 2: // DROP
-        dataFile.println(packetString);
-        Serial1.println(packetString);
-        break;
-      case 3: // DESCENT
-        dataFile.println(packetString);
-        Serial2.println(packetString);
-        break;
-      case 4: // LANDING
-        dataFile.close();
-      default: // IDLE
-        break;
-    };
+  // update mission_params
+  MISSION_PARAMS.MISSION_TIME = TIMESTAMP - START_TIME;
+  MISSION_PARAMS.PACKET_COUNT++;
+  MISSION_PARAMS.ALTITUDE = BMP.readAltitude(SEALEVELPRESSURE_HPA);// - ALT_START;
+  MISSION_PARAMS.PRESSURE = BMP.readPressure();
+  MISSION_PARAMS.TEMP = BMP.readTemperature();
+  MISSION_PARAMS.VOLTAGE = 0.0;
+  MISSION_PARAMS.GPS_LAT = GPS.latitude;
+  MISSION_PARAMS.GPS_LON = GPS.longitude;
+  packetString = csvParams(MISSION_PARAMS);
 
-    
-    // FS2 -> FS3 -> FS4
-    else if () {}
+  // main execution code
+  switch (MISSION_PARAMS.FLIGHT_STATE) {
+    case 0: // IDLE
+      Serial1.println(packetString);
+      MISSION_PARAMS.FLIGHT_STATE = 1;
+      #ifdef SERIAL_DEBUG
+        Serial.println(packetString);
+      #endif
+      // blink LED
+      break;
+    case 1: // ASCENT
+      Serial1.println(packetString);
+      Serial2.println(packetString);
+      #ifdef SERIAL_DEBUG
+        Serial.println(packetString);
+      #endif
+      // blink LED
+      
+      // FS1 -> FS2
+      if (MISSION_PARAMS.ALTITUDE > ALTITUDE_MAX) {
+        MISSION_PARAMS.FLIGHT_STATE = 2; // drop stage
+      }
+      break;
+    case 2: // DROP
+      Serial1.println(packetString);
+      Serial2.println(packetString);
+      #ifdef SERIAL_DEBUG
+        Serial.println(packetString);
+      #endif
+      // blink LED
 
-    // FS4 -> END
-    else if () {}
+      // fill in with servo code
 
-    // DEFAULT
-    else
-  }
+      // FS2 -> FS3
+      if (SERVO.read() > 1000) {
+        MISSION_PARAMS.FLIGHT_STATE = 3; // descent stage
+        MISSION_PARAMS.PL_STATE = 'R';   // update payload state to "Released"
+      }
+      break;
+    case 3: // DESCENT
+      Serial1.println(packetString);
+      Serial2.println(packetString);
+      #ifdef SERIAL_DEBUG
+        Serial.println(packetString);
+      #endif
+      // blink LED
 
+      // FS3 -> FS4
+      if (MISSION_PARAMS.ALTITUDE < 10) {
+        MISSION_PARAMS.FLIGHT_STATE = 4; // landing stage
+      }
+      break;
+    case 4: // LANDING
+      Serial1.println(packetString);
+      #ifdef SERIAL_DEBUG
+        Serial.println(packetString);
+      #endif
+
+      break;
+    default: // IDLE
+      #ifdef SERIAL_DEBUG
+        Serial.println(packetString);
+      #endif
+      break;
+  };
 }
 
 // create packet data
-String csvParams(struct MP) {
+String csvParams(struct params MP) {
   String c = ",";
-  return MP.TEAM_ID + c + MP.MISSION_TIME + c + MP.PACKET_COUNT + c + MP.FLIGHT_STATE + c + MP.PL_STATE + c + MP.ALTITUDE + c + MP.TEMP + c + MP.VOLTAGE + c + MP.GPS_LAT + c + MP.GPS_LON + "\n";
+
+  // strings to store data
+  char teamID[4];
+  char missionTime[11];
+  char packetCount[5];
+  char flightState[1];
+  char plState[1];
+  char altitude[5];
+  char temp[4];
+  char voltage[3];
+  char lat[10];
+  char lon[10];
+
+  return MP.TEAM_ID + c + MP.MISSION_TIME + c + MP.PACKET_COUNT + c + MP.FLIGHT_STATE + c + MP.PL_STATE + c + MP.ALTITUDE + c + MP.PRESSURE + c + MP.TEMP + c + MP.VOLTAGE + c + MP.GPS_LAT + c + MP.GPS_LON;
 }
 
